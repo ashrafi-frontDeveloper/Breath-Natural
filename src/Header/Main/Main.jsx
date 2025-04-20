acf96b356727759f8cba2f6630afd76cbf35993e
Mohammad Ashrafi <ashrafi.dev221184@gmail.com>
2025-04-20 16:36:19 +0330

    Coding: Create a shopping cart now

--- src/Header/Main/Main.jsx
+++ src/Header/Main/Main.jsx
@@ -1,11 +1,70 @@
 import React, { Component } from 'react'
 
 export default class Main extends Component {
+
   render() {
     return (
-      <div>
-        
-      </div>
+          <section className='container pt-14 mx-auto hidden md:flex items-center justify-between'>
+
+            {/* Buy now */}
+            <div className="relative flex flex-col items-center justify-end w-[300px] h-[356px] mt-12 rounded-[40px] bg-white/10 border-l border-r border-white backdrop-blur-md p-6 shadow-lg">
+
+              {/* img plants */}
+              <img src="image/main-header/img1.png" alt="Calathea plant" className="absolute -top-20" />
+
+              {/* info plants */}
+              <div className=' absolute text-white text-center h-full top-40'>
+                <span className='text-base opacity-70 mb-1'>Trendy House Plant</span>
+                
+                {/*  name plants */}
+                <h3 className='flex items-center text-xl gap-1'>
+                  Calathea plant
+                </h3>
+
+                {/*  btn buy */}
+                <button className='border border-white text-white px-6 py-2 rounded-2xl mt-3.5 hover:bg-white hover:text-green-800 transition'>
+                  Buy Now
+                </button>
+              </div>
+
+              {/* arrow-left */}
+              <button className='mb-4'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#fff" className="size-6">
+    <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+                </svg>
+              </button>
+
+              {/*  slider */}
+              <div className="absolute bottom-4 flex gap-1.5">
+                <span className="w-1.5 h-1.5 bg-white rounded-full opacity-80"></span>
+                <span className="w-1.5 h-1.5 bg-white rounded-full opacity-30"></span>
+                <span className="w-1.5 h-1.5 bg-white rounded-full opacity-30"></span>
+              </div>
+            </div>
+
+    </section>
+
     )
   }
 }
+
+      // <section className='container pt-14 mx-auto hidden md:flex items-center justify-between'>
+
+      //   {/* Buy now */}
+      //   <div class="relative flex items-center justify-center w-[300px] h-[356px] mt-12 rounded-4xl bg-white/10 border-l border-r border-white backdrop-blur-md p-6 shadow-lg">
+      //     <img src="image/main-header/img1.png" alt="" className="absolute -top-20" />
+      //     <div className='text-white text-2xl mt-10'>
+      //       <span className='text-base'>Trendy House Plant</span>
+      //       <h3 className='flex items-center justify-between'>
+      //         Calathea plant
+      //         <button>
+      //           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-6">
+      //             <path fillRule="evenodd" d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
+      //           </svg>
+      //         </button>
+      //       </h3>
+      //       <button className='border px-10 py-3.5 rounded-2xl mt-3.5 '>Buy Now</button>
+      //     </div>
+      //   </div>
+        
+      // </section>
