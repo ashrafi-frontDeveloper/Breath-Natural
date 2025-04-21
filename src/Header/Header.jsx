d671e9e6ebc415348a1e56f50b4e365b8841e5c4
Mohammad Ashrafi <ashrafi.dev221184@gmail.com>
2025-04-21 13:50:15 +0330

    Finishing the header section with the responsive

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -5,7 +5,8 @@ import Main from './Main/Main'
 export default class Header extends Component {
   render() {
     return (
-        <header className="bg-[url('/image/bg-header.png')] h-screen bg-cover w-full mb-96">
+        // <header className="bg-[url('/image/bg-header.png')] h-screen bg-cover w-full mb-96">
+        <header className="bg-[url('/image/bg-header.png')] bg-cover bg-center w-full min-h-screen">
           {/* Component */}
           <NavBar></NavBar>
           <Main></Main>
--- src/Header/Main/Main.jsx
+++ src/Header/Main/Main.jsx
@@ -31,65 +31,65 @@ export default class Main extends Component {
     const currentPlant = plants[currentIndex];
 
     return (
-          <section className='container pt-14 mx-auto hidden md:flex items-center justify-between'>
+          <section className='container pt-4 lg:pt-7 xl:pt-14 mx-auto flex-row md:flex md:items-center md:justify-between'>
             {/* title web site */}
-            <div className='text-white'>
+            <div className='text-white text-center md:text-start'>
                 {/* title */}
-                <h1 className="text-8xl font-bold">Breath Natureal</h1>
+                <h1 className="text-5xl lg:text-6xl xl:text-8xl mb-4 font-bold">Breath Natureal</h1>
                 {/* sub title */}
-                <p className="text-2xl">
+                <p className="md:w-[500px] text-base lg:text-[18px] xl:text-2xl">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 </p>
                 {/* explore */}
-                <div className="flex items-center gap-x-10 mt-10">
-                  <button className='border border-white py-3 px-10 rounded-2xl hover:bg-white hover:text-green-600 transition-all'>Explore</button>
+                <div className="flex items-center justify-center md:justify-start gap-x-3.5 lg:gap-x-6 xl:gap-x-10 mt-3.5 lg:mt-6 xl:mt-10">
+                  <button className='border border-white py-1 lg:py-1.5 xl:py-3 px-4 lg:px-7 xl:px-10 rounded-2xl hover:bg-white hover:text-green-600 transition-all'>Explore</button>
                   <button className='flex items-center gap-x-4'>
-                    <a href="#" className='border border-white rounded-full p-4 hover:bg-white hover:text-green-600 transition-all'>
+                    <a href="#" className='border border-white rounded-full p-1.5 lg:p-2 xl:p-4 hover:bg-white hover:text-green-600 transition-all'>
                       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
                         <path fillRule="evenodd" d="M4.5 5.653c0-1.427 1.529-2.33 2.779-1.643l11.54 6.347c1.295.712 1.295 2.573 0 3.286L7.28 19.99c-1.25.687-2.779-.217-2.779-1.643V5.653Z" clipRule="evenodd" />
                       </svg>
                     </a>
-                    <span className='text-2xl'>Live Demo...</span>
+                    <span className='text-[18px] xl:text-2xl font-bold md:font-light'>Live Demo...</span>
                   </button>
                 </div>
             </div>
 
-            {/* Buy now */}
-            <div className="relative flex flex-col items-center justify-end w-[300px] h-[356px] mt-12 rounded-[40px] bg-white/10 border-l border-r border-white backdrop-blur-md p-6 shadow-lg">
-
-              {/* img plants */}
-              <img src={currentPlant.img} alt="Calathea plant" className="absolute -top-20" />
-
-              {/* info plants */}
-              <div className=' absolute text-white text-center h-full top-40'>
-                <span className='text-base opacity-70 mb-1'>{currentPlant.title}</span>
+            {/* Buy cart */}
+            <div className="flex justify-center md:justify-end mt-10 md:mt-0">
+              {/*  Buy cart */}
+              <div className="relative flex md:flex-col items-center justify-center md:justify-end w-60 xl:w-[300px] h-72 xl:h-[356px] mt-16 xl:mt-12 rounded-[40px] bg-white/10 border-l border-r border-white backdrop-blur-md pt-10 xl:p-6 shadow-lg">
                 
-                {/*  name plants */}
-                <h3 className='flex items-center text-center text-xl gap-1'>
-                  {currentPlant.sunTitle}
-                </h3>
+                {/* img plants */}
+                <img src={currentPlant.img} alt="Calathea plant" className="absolute -top-20" />
 
-                {/*  btn buy */}
-                <button className='border border-white text-white px-6 py-2 rounded-2xl mt-3.5 hover:bg-white hover:text-green-800 transition'>
-                  Buy Now
+                {/* info plants */}
+                <div className='absolute text-white text-center h-full top-30 xl:top-40'>
+                  <span className='text-base opacity-70 mb-1'>{currentPlant.title}</span>
+                  <h3 className='flex items-center text-center text-xl gap-1'>
+                    {currentPlant.sunTitle}
+                  </h3>
+                  <button className='border border-white text-white px-6 py-2 rounded-2xl mt-3.5 hover:bg-white hover:text-green-800 transition'>
+                    Buy Now
+                  </button>
+                </div>
+
+                {/* arrow-left */}
+                <button onClick={this.clickHandler} className='absolute -mb-40 md:mb-8 xl:mb-4'>
+                  <svg fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#fff" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+                  </svg>
                 </button>
-              </div>
 
-
-              {/* arrow-left */}
-              <button onClick={this.clickHandler} className=' absolute mb-4'>
-                <svg fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#fff" className="size-6">
-    <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
-                </svg>
-              </button>
-
-              {/*  slider */}
-              <div className="absolute bottom-4 flex gap-1.5">
-                <span className="w-1.5 h-1.5 bg-white rounded-full opacity-80"></span>
-                <span className="w-1.5 h-1.5 bg-white rounded-full opacity-30"></span>
-                <span className="w-1.5 h-1.5 bg-white rounded-full opacity-30"></span>
+                {/* slider */}
+                <div className="absolute bottom-4 flex gap-1.5">
+                  <span className="w-1.5 h-1.5 bg-white rounded-full opacity-80"></span>
+                  <span className="w-1.5 h-1.5 bg-white rounded-full opacity-30"></span>
+                  <span className="w-1.5 h-1.5 bg-white rounded-full opacity-30"></span>
+                </div>
               </div>
             </div>
+
+
 
     </section>
 
