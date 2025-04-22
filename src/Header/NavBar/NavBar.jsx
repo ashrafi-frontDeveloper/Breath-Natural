27bd2a2a611479dc655d915829ec8311b2a7ad63
Mohammad Ashrafi <ashrafi.dev221184@gmail.com>
2025-04-22 19:27:06 +0330

    Coding update navbar , add login / sign up

--- src/Header/NavBar/NavBar.jsx
+++ src/Header/NavBar/NavBar.jsx
@@ -130,6 +130,7 @@ export default function NavBar() {
 
         {/* Icons */}
         <div className='flex gap-x-7 lg:gap-x-14'>
+          {/* search */}
             <form action="" className='flex gap-x-2'>
                 <input type="text" placeholder="Typing ..." className="text-white px-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500 transition duration-300"/>
                 <a href="#" className='hover:scale-110 transition duration-300'>
@@ -138,10 +139,18 @@ export default function NavBar() {
                     </svg>
                 </a>
             </form>
+            {/* basket */}
             <a href="#" className='hover:scale-110 transition duration-300'>
             <svg fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#fff" className="size-6">
   <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
 </svg>
+            </a>
+            {/* login / sign up */}
+            <a href="#" className='hover:scale-105 transition duration-300 flex items-center gap-1 text-white'>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+              </svg>
+              <span>Login / Sign up</span>
             </a>
         </div>
       </nav>
