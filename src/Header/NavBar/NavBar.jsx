671b50b85670f2afa1bd76ae8d3d5db25607ef1e
Mohammad Ashrafi <ashrafi.dev221184@gmail.com>
2025-04-30 19:44:04 +0330

    Update "fix: image path issue"

--- src/Header/NavBar/NavBar.jsx
+++ src/Header/NavBar/NavBar.jsx
@@ -122,7 +122,7 @@ export default function NavBar() {
       <nav data-aos="fade-down" className=' container mx-auto relative z-[999] pt-14 hidden md:flex items-center justify-between'>
         {/* Logo */}
         <div className='flex items-center text-white gap-x-1.5 lg:gap-x-3.5 text-[18px] lg:text-2xl'>
-          <img className='lg:w-12 w-7 lg:h-12 h-7' src="./svg/navbar/plant.svg" alt="logo" />
+          <img className='lg:w-12 w-7 lg:h-12 h-7' src="/Breath-Natural/svg/navbar/plant.svg" alt="logo" />
           <span className='font-bold font-inter text-base'>Planto.</span>
         </div>
 
@@ -158,7 +158,7 @@ export default function NavBar() {
       {/* Mobile Navbar */}
       <nav className='container mx-auto pt-14 flex items-center justify-between md:hidden'>
         <div className='flex items-center text-white gap-x-1.5 text-[18px] lg:text-2xl'>
-          <img className='lg:w-12 w-7 lg:h-12 h-7' src="./svg/navbar/plant.svg" alt="logo" />
+          <img className='lg:w-12 w-7 lg:h-12 h-7' src="/Breath-Natural/svg/navbar/plant.svg" alt="logo" />
           <span className='font-bold font-inter text-base'>Planto.</span>
         </div>
 
